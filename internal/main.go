697411f45b19d5c9335c1392a4244691e5512902
Rouge <public.syl@outlook.com>
2025-05-04 17:18:33 +0800

    webook-fe 与后端对上了

--- internal/main.go
+++ internal/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 	"webook_Rouge/internal/web"
 )
 
 func main() {
 	server := gin.Default()
-	// 静态路由
+
+	// 解决跨域问题
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"}, //
+		// AllowMethods: []string{"POST", "GET"}, // 不写的话默认的几个简单方法都ok
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		// ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			// 开发环境判断
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			// 如果需要，加上生产环境判断，例如公司域名
+			return false
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	u := &web.UserHandler{}
 
 	u.RegisterRoutes(server)
--- internal/web/user.go
+++ internal/web/user.go
@@ -1,22 +1,40 @@
 package web
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type UserHandler struct {
 }
 
 func (handler *UserHandler) RegisterRoutes(server *gin.Engine) {
-	server.POST("/users/signup", u.SignUp) // 注册
-	server.POST("/users/login", u.Login)   // 登录
-	server.POST("/users/edit", u.Edit)     // 编辑
+	server.POST("/users/signup", handler.SignUp) // 注册
+	server.POST("/users/login", handler.Login)   // 登录
+	server.POST("/users/edit", handler.Edit)     // 编辑
 
-	server.GET("/users/profile", u.Profile) // 查看个人信息
+	server.GET("/users/profile", handler.Profile) // 查看个人信息
 
 }
-func (handler *UserHandler) SignUp(ctx *gin.Context) {
 
+func (handler *UserHandler) SignUp(ctx *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
+
+	var req SignUpReq
+
+	// Bind 方法会根据 Content-Type 来解析数据进 req 中
+	// 若解析错误，会写入 400 的HTTP状态码
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	fmt.Printf("%v\n", req)
+	ctx.String(http.StatusOK, "已收到您的注册请求\n")
 }
 
 func (handler *UserHandler) Login(ctx *gin.Context) {
